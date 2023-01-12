bf26a900868bafbeb8f2c9941b7671762d73fd12
elmiado <sayad.mahdi75@gmail.com>
2023-01-12 19:44:33 +0330

    adding StorageFactory.sol

---
+++ contracts/StorageFactory.sol
@@ -0,0 +1,32 @@
+// SPDX-License_identifier: MIT
+
+pragma solidity ^0.6.0;
+
+import "./SimpleStorage.sol";
+
+// inheritance is avaliable in solidity with this syntax
+contract StorageFactory is SimpleStorage{
+    // we deply this contract and use another ontract from blockchain
+    // you can define below variable private too for a little professional and better result,
+    SimpleStorage[] public simpleStorageArray;
+    function createSimpleStorageContract() public {
+        SimpleStorage simpleStorage = new SimpleStorage();
+        simpleStorageArray.push(simpleStorage);
+    }
+
+    function sfStore(uint256 _simpleStorageIndex, uint256 _simpleStorageNumber) public {
+        // address
+        // ABI Application Binary Interface
+        // SimpleStorage simpleStorage = SimpleStorage(address(simpleStorageArray[_simpleStorageIndex]));
+        // simpleStorage.store(_simpleStorageNumber);
+        // actually we can do this in another way
+        SimpleStorage(address(simpleStorageArray[_simpleStorageIndex])).store(_simpleStorageNumber);
+    }
+
+    function sfGet(uint256 _simpleStorageIndex) public view returns (uint256) {
+        // SimpleStorage simpleStorage = SimpleStorage(address(simpleStorageArray[_simpleStorageIndex]));
+        // return simpleStorage.get();
+        // actually we can do this in another way
+        return SimpleStorage(address(simpleStorageArray[_simpleStorageIndex])).get();
+    }
+}
